111f3d5cfe2d90fe52271af90613435e21b835d4
Satasuk Vipaksinlapin <satasuk@gmail.com>
2017-10-10 22:00:35 +0700

    Make the ball can bounce and launch the ball from the paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private Vector3 paddleToBallVector;
+	bool hasStarted=false;//To make sure that if we lauched the ball the ball won't be attached at the paddle anymore.
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position; //distance between ball and paddle(Y axis)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStarted) {
+			//Lock the ball relative to the paddle.
+			this.transform.position = paddle.transform.position + paddleToBallVector; //because the ball is on paddle(not in the paddle)
+			//we have to set script execution order paddle first and then ball (make the ball attached to paddle or it will be detach or glitch when we move the paddle fast)
+
+			//wait for a mouse press to launch.
+			if (Input.GetMouseButtonDown (0)) {//0 for left 1 for right 2 for middle
+				Debug.Log ("Left clicked");
+				hasStarted = true;
+				//this.rigidbody2D.velocity = new Vector2 (2f, 10f); can't use on newer version of unity
+				this.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print (Input.mousePosition);
+		float mousePosInBlock;
+		mousePosInBlock=Input.mousePosition.x/Screen.width*16;//give [0,1] multiply by 16 because of 16 world unit wide
+		//print(mousePosInBlock);
+		//this.transform.position.x = mousePosInBlock;  Unity won't let we change x directly we must change at position by Vector3
+		Vector3 paddlePos = new Vector3(0.5f,this.transform.position.y,this.transform.position.z); //start at left , will this var declare as new obj every frame???
+		paddlePos.x = Mathf.Clamp(mousePosInBlock,0.5f,15.5f); //set minimum pos to 0.5 and maximum to 15.5
+		this.transform.position = paddlePos;
+	}
+}
